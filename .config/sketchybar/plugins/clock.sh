f45938036f433dcdae77c12024d80bedc3083e57
ofietze <f.oliver@live.de>
2025-08-25 19:06:35 +0200

    Add clock

--- .config/sketchybar/plugins/clock.sh
+++ .config/sketchybar/plugins/clock.sh
@@ -4,5 +4,22 @@
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')"
+#!/bin/sh
+
+# Function to get ordinal suffix
+get_ordinal() {
+    day=$1
+    case $day in
+        1|21|31) echo "${day}st" ;;
+        2|22) echo "${day}nd" ;;
+        3|23) echo "${day}rd" ;;
+        *) echo "${day}th" ;;
+    esac
+}
+
+day=$(date '+%-d')
+ordinal_day=$(get_ordinal $day)
+formatted_date=$(date '+%a')
+
+sketchybar --set "$NAME" label="$formatted_date $ordinal_day $(date '+%B %H:%M')"
 
