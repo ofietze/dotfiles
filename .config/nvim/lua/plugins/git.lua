2ba9fdf48c0e8460914040462ef27dc4ab392003
Oliver Fietze <f.oliver@live.de>
2025-08-26 10:01:53 +0200

    Add gitsigns and update newsboat

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,20 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("gitsigns").setup({
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 500,
+        virt_text_pos = "eol",
+      },
+    })
+  end,
+}
